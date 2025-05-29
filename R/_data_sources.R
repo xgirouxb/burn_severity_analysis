3b76c4ebd949e96b4ff661e9979f4a0d833ec449
xgirouxb <xgirouxb@gmail.com>
2025-05-29 10:07:16 -0400

    Add data sources script for reproducibility

---
+++ R/_data_sources.R
@@ -0,0 +1,5 @@
+# DATA SOURCES
+
+# ---------------------------------------------------------------------------- #
+# Matt Heathcoat's raw BC burn severity dataset 
+path_fire_data <- "data/raw_burn_data/BCfiresDF.csv"
