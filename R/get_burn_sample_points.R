6bb71f0286fed5437963ea8b400b41717bf4dbbc
xgirouxb <xgirouxb@gmail.com>
2025-05-30 17:20:37 -0400

    Fix old fire IDs
    This removes leading 0s in the old fire IDs so they match current fire IDs in the NBAC archives.

--- R/get_burn_sample_points.R
+++ R/get_burn_sample_points.R
@@ -4,8 +4,20 @@ get_bc_burn_sample_points <- function(path_fire_data){
   raw_data <- readr::read_csv(path_fire_data, show_col_types = FALSE) %>% 
     # Clean up 
     janitor::clean_names() %>% 
+    # Parse old and new fire IDs (MGH has trailing 0s that are removed in NBAC)
+    dplyr::mutate(
+      old_fire_id = fire_id,
+      fire_id = paste0(
+        # Keep original year suffix
+        fire_year, "_",
+        # Remove leading 0s from NBAC fire id
+        readr::parse_number(stringr::str_replace(old_fire_id, ".*_", ""))
+      )
+    ) %>% 
     # Select columns of interest for downstream sampling
-    dplyr::select(id = oid, fire_id, fire_year, x_3005, y_3005)
+    dplyr::select(id = oid, fire_id, old_fire_id, fire_year, x_3005, y_3005) %>%
+    # Testing
+    {.}
   
   # Convert to simple feature
   burn_sample_points <- raw_data %>% 
