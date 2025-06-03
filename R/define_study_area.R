a8b9cf4e383c44a490ecb6f5a50c510850da66f3
xgirouxb <xgirouxb@gmail.com>
2025-06-03 13:07:00 -0400

    Fix output
    Ensure target is sf object, not just geometry.

--- R/define_study_area.R
+++ R/define_study_area.R
@@ -9,12 +9,12 @@ define_study_area <- function() {
   study_area <- statcan_admin_bounds %>%
     # Filter British Columbia
     dplyr::filter(PRUID == 59) %>% 
-    # Cast MULTIPOLYGON to POLYGON
-    sf::st_cast("POLYGON", warn = FALSE) %>%
     # Fix topology errors
     sf::st_make_valid() %>%
     # Combine into single layer
-    sf::st_union() 
+    sf::st_union() %>% 
+    # Cast to sf
+    sf::st_as_sf()
   
   # Return
   return(study_area)
