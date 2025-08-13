b15a8b5f8b9e543d5fae9c11154aa1a67e8fdab4
xgirouxb <xgirouxb@gmail.com>
2025-08-13 15:44:55 -0400

    Update selection of study fires
    Programmatically exclude fires that are not included in MGH samples.

--- R/get_study_fire_polygons.R
+++ R/get_study_fire_polygons.R
@@ -1,9 +1,14 @@
 get_study_fire_polygons <- function(study_area, study_years) {
   
+  # -------------------------------------------------------------------------- #
+  # Step 1: Import and wrangle fire polygons from NBAC archive              ####
+  
   # Get NBAC archive URLs for study years
   list_nbac_subdir <- purrr::map(
       .x = study_years,
-      .f = ~{ get_url_list(url = url_nbac_archive, match_string = paste0(.x)) }
+      .f = ~{ 
+        get_url_list(url = url_nbac_archive, match_string = paste0("NBAC_", .x)) 
+      }
   )
   
   # Get NBAC fire polygons that intersect with study area
@@ -16,11 +21,9 @@ get_study_fire_polygons <- function(study_area, study_years) {
   nbac_fire_polygons <- dplyr::bind_rows(list_nbac_fire_polygons) %>%
     # Make a unique fire_id
     dplyr::mutate(fire_id = paste0(YEAR, "_", NFIREID)) %>%
-    # Remove 2 fires that are not in MGH samples
-    dplyr::filter(!(fire_id %in% c("2017_1002", "2018_1291"))) %>%
     # Remove prescribed burns ("2017_2418)
     dplyr::filter(PRESCRIBED != "true" | is.na(PRESCRIBED)) %>%
-    # Remove fire not within BC ("2018_35")
+    # Remove fires not within BC ("2018_35")
     sf::st_filter(study_area, .predicate = sf::st_within) %>%
     # Dissolve into single MULTIPOLYGON for each fire
     dplyr::group_by(fire_id) %>%
@@ -39,6 +42,26 @@ get_study_fire_polygons <- function(study_area, study_years) {
     sf::st_make_valid() %>%
     sf::st_cast("MULTIPOLYGON")
   
+  # -------------------------------------------------------------------------- #
+  # Step 2: Remove fires without samples in MGH dataset                     ####
+  #         NB This step may eventually be removed if all sampling is 
+  #            integrated into this {targets} pipeline
+  
+  # Get and parse fire_ids in MGH
+  mgh_fire_ids <- readr::read_csv(path_fire_data, show_col_types = FALSE) %>%
+    # Split string, remove leading 0s, then combine
+    dplyr::distinct(fireID) %>% 
+    tidyr::separate(fireID, into = c("year", "id"), sep = "_") %>%
+    dplyr::mutate(id = as.integer(id)) %>%  
+    tidyr::unite("fire_id", year, id, sep = "_") %>% 
+    # Grab vector
+    dplyr::pull(fire_id)
+  
+  # Remove fires that don't have corresponding samples in MGH dataset
+  # "2017_1002", "2018_1291", and "2018_194"
+  study_fire_polygons <- nbac_fire_polygons %>% 
+    dplyr::filter(!(fire_id %in% setdiff(.$fire_id, mgh_fire_ids)))
+  
   # Return 
-  return(nbac_fire_polygons)
+  return(study_fire_polygons)
 }
