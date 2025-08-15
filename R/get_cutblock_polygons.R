dd8d3780e405433219f58ffbb29d4519e0073738
xgirouxb <xgirouxb@gmail.com>
2025-08-15 12:59:13 -0400

    Add "cc_" prefix to output cutblock attributes

--- R/get_cutblock_polygons.R
+++ R/get_cutblock_polygons.R
@@ -1,17 +1,14 @@
 get_cutblock_polygons <- function(study_fire_sampling_polygons){
-  
-  # Define consolidated cutblock archive
-  cutblock_polygons_archive <- bcdata::bcdc_query_geodata(uuid_bc_cutblocks)
-  
-  # Import intersections of BC consolidated cutblocks and study fires
+
+  # Import BC consolidated cutblocks that intersect and predate study fires
   cutblock_polygons <- study_fire_sampling_polygons %>%
     # Nest by fire
-    dplyr::group_split(fire_id, fire_year, fire_start_date, fire_end_date) %>%
+    dplyr::group_split(fire_id, fire_year) %>%
     # Import cutblocks that intersect with individual fires
     purrr::map(
       .f = ~{
         # Spatial intersection filter of fire sampling polygons
-        cutblocks_retrieved <- cutblock_polygons_archive %>%
+        cutblocks_retrieved <- bcdata::bcdc_query_geodata(uuid_bc_cutblocks) %>%
           dplyr::filter(bcdata::INTERSECTS(.x)) %>%
           bcdata::collect() 
         
@@ -25,10 +22,20 @@ get_cutblock_polygons <- function(study_fire_sampling_polygons){
           # Add fire attributes
           dplyr::mutate(
             fire_id = .x$fire_id,
-            fire_year = .x$fire_year,
-            fire_start_date = .x$fire_start_date,
-            fire_end_date = .x$fire_end_date,
-          ) %>% 
+            fire_year = .x$fire_year
+          ) %>%
+          # Coerce datatypes to fix errors when some tibble columns are all NAs
+          dplyr::mutate(
+            vccb_sysid = as.integer(vccb_sysid),
+            opening_id = as.integer(opening_id),
+            harvest_start_date = lubridate::as_date(harvest_start_date),
+            harvest_end_date = lubridate::as_date(harvest_end_date),
+            harvest_start_year = as.integer(harvest_start_year_calendar),
+            harvest_end_year = as.integer(lubridate::year(harvest_end_date)),
+            percent_clearcut = as.integer(percent_clearcut),
+            percent_partial_cut = as.integer(percent_partial_cut),
+            area_ha = as.integer(area_ha)
+          ) %>%            
           # Select cutblock attributes of interest
           dplyr::select(
             # Fire IDs
@@ -37,27 +44,25 @@ get_cutblock_polygons <- function(study_fire_sampling_polygons){
             vccb_sysid, opening_id,
             # Time
             harvest_start_date, harvest_end_date,
-            harvest_start_year = harvest_start_year_calendar,
+            harvest_start_year, harvest_end_year,
             # Cutblock attributes
             percent_clearcut, percent_partial_cut, area_ha,
             # Source
             data_source, data_source_date,
             # Geometry
             geometry
-          ) %>% 
-          # Coerce datatypes to fix errors when some tibble columns are all NAs
-          dplyr::mutate(
-            vccb_sysid = as.integer(vccb_sysid),
-            opening_id = as.integer(opening_id),
-            harvest_start_date = lubridate::as_date(harvest_start_date),
-            harvest_end_date = lubridate::as_date(harvest_end_date),
-            harvest_start_year = as.integer(harvest_start_year),
-            percent_clearcut = as.integer(percent_clearcut),
-            percent_partial_cut = as.integer(percent_partial_cut),
-            area_ha = as.integer(area_ha)
           )
       }
     ) %>%
     # Unnest
-    dplyr::bind_rows()
+    dplyr::bind_rows() %>% 
+    # Add consolidated cutblock prefix to variable names
+    dplyr::rename_with(
+      ~ paste0("cc_", .x),
+      # Exclude IDs, geometry, and variables that already have the prefix
+      !(fire_year | fire_id | geometry)
+    )
+  
+  # Return
+  return(cutblock_polygons)
 }
