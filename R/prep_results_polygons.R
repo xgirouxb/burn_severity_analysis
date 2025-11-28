795faf5de22f82604578176c356ba7d075acf514
xgirouxb <xgirouxb@gmail.com>
2025-11-28 09:51:05 -0500

    Update comments

--- R/prep_results_polygons.R
+++ R/prep_results_polygons.R
@@ -301,8 +301,8 @@ prep_results_polygons <- function(
       .groups = "drop"
     ) %>% 
     # Pool rare species
-    # NB: Not all these are planted species, they are included here for  
-    #     consistency with VRI pooling method
+    # NB: Not all these are planted species, they are included here verbatim  
+    #     from the VRI pooling method for consistency
     dplyr::mutate(
       res_lead_spp = dplyr::case_when(
         # Other deciduous
