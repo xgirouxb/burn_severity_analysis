76e9aa1167ccd20805bd4431d36e6be581e285a2
xgirouxb <xgirouxb@gmail.com>
2025-12-16 15:09:33 -0500

    Update import CanLaD disturbance raster target
    For syntax clarity/consistency, use`purrr::map` instead of `purrr::map2` and anonymous function declarations instead of shorthand. For efficiency, use `terra` built-in function "max" instead of custom helper NA-proof "maximum" as it already handles NAs.

--- R/get_canlad_rasters.R
+++ R/get_canlad_rasters.R
@@ -1,6 +1,4 @@
-get_canlad_disturbance_rasters <- function(
-    study_fire_sampling_polygons
-){
+get_canlad_disturbance_rasters <- function(study_fire_sampling_polygons) {
   
   # Define CanLaD years to download (year preceding most recent study fire)
   canlad_years <- 1985:(max(study_years) - 1)
@@ -25,7 +23,7 @@ get_canlad_disturbance_rasters <- function(
   if (length(uncached_raster_urls) > 0) {
     purrr::walk(
       uncached_raster_urls,
-      ~get_tif_from_url(.x, output_dir = raw_canlad_cache)
+      function(url) { get_tif_from_url(url, output_dir = raw_canlad_cache) }
     )
   }
   
@@ -37,90 +35,90 @@ get_canlad_disturbance_rasters <- function(
   # Get CanLaD projection (Canada Lambert)
   canlad_proj <- terra::crs(terra::rast(raster_paths[1]))
   
-  # Define NA proof max helper function
-  maximum <- function(r) ifelse(all(is.na(r)), NA, max(r, na.rm = TRUE))
-  
   # Create list of CanLaD most recent harvest and fire years for each study fire
-  canlad_harvest_fire_files <- study_fire_sampling_polygons %>% 
-    # Nest by study fire
-    dplyr::group_nest(fire_id) %>% 
+  canlad_disturbance_paths <- study_fire_sampling_polygons %>% 
+    # Split by study fire
+    dplyr::group_split(fire_id) %>% 
     # Get year of most recent harvest and most recent wildfire in CanLaD stack
-    dplyr::mutate(
-      canlad_file_path = purrr::map2(
-        data,
-        fire_id,
-        ~{
-          # Define year range for CanLaD stack (1985 to 1 year before study fire)
-          years <- 1985:(max(.x$fire_year) - 1)
-
-          # Subset CanLaD raster stack paths
-          stack_paths <- stringr::str_subset(
-            raster_paths,
-            paste(years, collapse = "|")
-          )
-
-          # Reproject study fire polygon
-          fire_lambert <- terra::project(terra::vect(.x), canlad_proj)
-
-          # Load, and crop/mask each CanLaD annual raster to study fire area
-          list_canlad_annual_rasters <- purrr::map(
-            stack_paths,
-            ~ terra::crop(x = terra::rast(.x), y = fire_lambert, mask = TRUE)
-          )
-          
-          # Get the year of most recent CanLaD fire disturbance in stack
-          canlad_fire_year <- purrr::map(
-            list_canlad_annual_rasters,
-            # Substitute annual wildfire pixels (1) with year
-            ~ terra::ifel(.x == 1, readr::parse_number(names(.x)), NA)
-          ) %>%
-            # Stack annual rasters, then compute pixel-wise max of wildfire years
-            terra::rast() %>%
-            terra::app(maximum)
-          names(canlad_fire_year) <- "canlad_fire_year"
-
-          # Get the year of most recent CanLaD harvest disturbance in stack
-          canlad_harvest_year <- purrr::map(
-            list_canlad_annual_rasters,
-            # Substitute annual harvest pixels (2) with year
-            ~ terra::ifel(.x == 2, readr::parse_number(names(.x)), NA)
-          ) %>%
-            # Stack annual rasters, then compute pixel-wise max of harvest years
-            terra::rast() %>%
-            terra::app(maximum)
-          names(canlad_harvest_year) <- "canlad_harvest_year"
-
-          # File name and path for writing to file
-          canlad_file_name <- fs::path(canlad_cache, paste0(.y, ".tif"))
-          
-          # Write fire and harvest disturbance years to file  
-          terra::writeRaster(
-            # Create 2 layer raster and reproject in study proj
-            x = terra::project(
-              x = c(canlad_fire_year, canlad_harvest_year),
-              y = study_proj,
-              res = 30
-            ), 
-            filename = canlad_file_name,
-            datatype = "INT2U",
-            overwrite = TRUE
-          )
-          
-          # Return file name
-          canlad_file_name
-        }
-      )
-    ) %>%
-    # Clean-up
-    dplyr::select(-data)
+    purrr::map(
+      function(study_fire) {
+        
+        # Study fire attributes
+        fire_year <- study_fire$fire_year
+        fire_id <- study_fire$fire_id
+        
+        # Define year range for CanLaD stack (1985 to 1 year before study fire)
+        years <- 1985:(max(fire_year) - 1)
+        
+        # Subset CanLaD raster stack paths
+        stack_paths <- stringr::str_subset(
+          raster_paths,
+          paste(years, collapse = "|")
+        )
+        
+        # Load, and crop/mask each CanLaD annual raster to study fire area
+        list_canlad_annual_rasters <- purrr::map(
+          stack_paths,
+          function(path) {
+            terra::crop(
+              x = terra::rast(path),
+              # Crop to reprojected study fires to CanLaD Lambert proj
+              y = terra::project(terra::vect(study_fire), canlad_proj),
+              mask = TRUE
+            )
+          }
+        )
+        
+        # Get the year of most recent CanLaD fire disturbance in stack
+        canlad_fire_year <- purrr::map(
+          list_canlad_annual_rasters,
+          function(r) { terra::ifel(r == 1, readr::parse_number(names(r)), NA) }
+        ) %>%
+          # Stack annual rasters, then compute pixel-wise max of wildfire years
+          terra::rast() %>%
+          terra::app(fun = "max", na.rm = TRUE)
+        names(canlad_fire_year) <- "canlad_fire_year"
+        
+        # Get the year of most recent CanLaD harvest disturbance in stack
+        canlad_harvest_year <- purrr::map(
+          list_canlad_annual_rasters,
+          # Substitute annual harvest pixels (2) with year
+          function(r) { terra::ifel(r == 2, readr::parse_number(names(r)), NA) }
+        ) %>%
+          # Stack annual rasters, then compute pixel-wise max of harvest years
+          terra::rast() %>%
+          terra::app(fun = "max", na.rm = TRUE)
+        names(canlad_harvest_year) <- "canlad_harvest_year"
+        
+        # File name and path for writing raster to file
+        raster_file_path <- fs::path(canlad_cache, paste0(fire_id, ".tif"))
+        
+        # Write fire and harvest disturbance years to file  
+        terra::writeRaster(
+          # Create 2 layer raster and reproject in study proj
+          x = terra::project(
+            x = c(canlad_fire_year, canlad_harvest_year),
+            y = study_proj,
+            res = 30
+          ), 
+          filename = raster_file_path,
+          datatype = "INT2U",
+          overwrite = TRUE
+        )
+        
+        # Return tbl of raster file path
+        tibble::tibble(
+          fire_id = fire_id,
+          raster_file_path = raster_file_path
+        )
+      }
+    )
   
   # Return list of CanLaD disturbance raster file names
-  return(canlad_harvest_fire_files)
+  return(canlad_disturbance_paths)
 }
 
-get_precanlad_disturbance_rasters <- function(
-    study_fire_sampling_polygons
-){
+get_precanlad_disturbance_rasters <- function(study_fire_sampling_polygons) {
   
   # Define local cache directory for pre-CanLaD rasters
   precanlad_cache <- fs::path("data/_cache/precanlad")
@@ -146,7 +144,7 @@ get_precanlad_disturbance_rasters <- function(
   if (length(uncached_raster_urls) > 0) {
     purrr::walk(
       uncached_raster_urls,
-      ~get_tif_from_url(.x, output_dir = raw_precanlad_cache)
+      function(url) { get_tif_from_url(url, output_dir = raw_precanlad_cache) }
     )
   }
   
@@ -159,66 +157,68 @@ get_precanlad_disturbance_rasters <- function(
   precanlad_proj <- terra::crs(terra::rast(raster_paths[1]))
   
   # Create list of pre-CanLaD harvest and fire years for each study fire
-  precanlad_harvest_fire_files <- study_fire_sampling_polygons %>% 
-    # Nest by study fire
-    dplyr::group_nest(fire_id) %>% 
+  precanlad_disturbance_paths <- study_fire_sampling_polygons %>% 
+    # Split by study fire
+    dplyr::group_split(fire_id) %>% 
     # Get year of harvest and wildfire disturbances in pre-CanLaD image
-    dplyr::mutate(
-      precanlad_file_path = purrr::map2(
-        data,
-        fire_id,
-        ~{
-          # Reproject study fire polygon
-          fire_lambert <- terra::project(terra::vect(.x), precanlad_proj)
-          
-          # Load, mask, and reproject pre-CanLaD disturbance years
-          disturbance_year <- stringr::str_subset(raster_paths, "Year") %>% 
-            terra::rast() %>%
-            terra::crop(y = fire_lambert, mask = TRUE)
-          
-          # Load, mask, and reproject pre-CanLaD disturbance years
-          disturbance_type <- stringr::str_subset(raster_paths, "Type") %>% 
-            terra::rast() %>%
-            terra::crop(y = fire_lambert, mask = TRUE)
-          
-          # Create raster with years of harvest disturbances pixels  (2)
-          precanlad_fire_year <- terra::mask(
-            x = disturbance_year,
-            mask = terra::ifel(disturbance_type == 2, 1, NA)
-          )
-          names(precanlad_fire_year) <- "precanlad_fire_year"
-          
-          # Create raster with years of harvest disturbances pixels  (3)
-          precanlad_harvest_year <- terra::mask(
-            x = disturbance_year,
-            mask = terra::ifel(disturbance_type == 3, 1, NA)
-          )
-          names(precanlad_harvest_year) <- "precanlad_harvest_year"
-          
-          # File name and path for writing to file
-          precanlad_file_name <- fs::path(precanlad_cache, paste0(.y, ".tif"))
-          
-          # Write fire and harvest disturbance years to file  
-          terra::writeRaster(
-            # Create 2 layer raster and reproject in study proj
-            x = terra::project(
-              x = c(precanlad_fire_year, precanlad_harvest_year),
-              y = study_proj,
-              res = 30
-            ),
-            filename = precanlad_file_name,
-            datatype = "INT2U",
-            overwrite = TRUE
-          )
-          
-          # Return file name
-          precanlad_file_name
-        }
-      )
-    ) %>%
-    # Clean-up
-    dplyr::select(-data)
+    purrr::map(
+      function(study_fire) {
+        
+        # Study fire attributes
+        fire_year <- study_fire$fire_year
+        fire_id <- study_fire$fire_id
+        
+        # Reproject fire to CanLaD projection
+        fire_lambert <- terra::project(terra::vect(study_fire), precanlad_proj)
+        
+        # Mask pre-CanLaD disturbance years to study fire
+        disturbance_year <- stringr::str_subset(raster_paths, "Year") %>% 
+          terra::rast() %>%
+          terra::crop(y = fire_lambert, mask = TRUE)
+        
+        # Mask pre-CanLaD disturbance type to study fire
+        disturbance_type <- stringr::str_subset(raster_paths, "Type") %>% 
+          terra::rast() %>%
+          terra::crop(y = fire_lambert, mask = TRUE)
+        
+        # Create raster with years of fire disturbances pixels  (2)
+        precanlad_fire_year <- terra::mask(
+          x = disturbance_year,
+          mask = terra::ifel(disturbance_type == 2, 1, NA)
+        )
+        names(precanlad_fire_year) <- "precanlad_fire_year"
+        
+        # Create raster with years of harvest disturbances pixels  (3)
+        precanlad_harvest_year <- terra::mask(
+          x = disturbance_year,
+          mask = terra::ifel(disturbance_type == 3, 1, NA)
+        )
+        names(precanlad_harvest_year) <- "precanlad_harvest_year"
+        
+        # File name and path for writing to file
+        raster_file_path <- fs::path(precanlad_cache, paste0(fire_id, ".tif"))
+        
+        # Write fire and harvest disturbance years to file  
+        terra::writeRaster(
+          # Create 2 layer raster and reproject to study proj
+          x = terra::project(
+            x = c(precanlad_fire_year, precanlad_harvest_year),
+            y = study_proj,
+            res = 30
+          ),
+          filename = raster_file_path,
+          datatype = "INT2U",
+          overwrite = TRUE
+        )
+        
+        # Return tbl of raster file path
+        tibble::tibble(
+          fire_id = fire_id,
+          raster_file_path = raster_file_path
+        )
+      }
+    )
   
   # Return list of pre-CanLaD disturbance raster file names
-  return(precanlad_harvest_fire_files)
+  return(precanlad_disturbance_paths)
 }
