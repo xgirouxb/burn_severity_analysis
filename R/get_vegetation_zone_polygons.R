6c27b24df5355c8348f495311e7278def3e68373
xgirouxb <xgirouxb@gmail.com>
2025-12-22 13:28:29 -0500

    Use unique() instead of dplyr::first()
    Substitute first() with unique() to add point of failure if there is more than one value per group.

--- R/get_vegetation_zone_polygons.R
+++ R/get_vegetation_zone_polygons.R
@@ -8,9 +8,9 @@ get_vegetation_zone_polygons <- function(study_fire_sampling_polygons) {
     # Group by Level 1 vegetation zone
     dplyr::group_by(id_1) %>% 
     # Dissolve into single MULTIPOLYGON for each vegetation zone
-    dplyr::summarize(
-      cvz = dplyr::first(id_1),
-      cvz_name = dplyr::first(level_1),
+    dplyr::summarise(
+      cvz = unique(id_1),
+      cvz_name = unique(level_1),
       geometry = sf::st_union(geometry),
       .groups = "drop"
     ) %>%
