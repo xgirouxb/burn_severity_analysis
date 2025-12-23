09df11169da3ed09615b6c2051d652e81d683d65
xgirouxb <xgirouxb@gmail.com>
2025-12-23 10:40:02 -0500

    Rename biogeoclimatic variables for consistency

--- R/get_biogeoclimatic_zone_polygons.R
+++ R/get_biogeoclimatic_zone_polygons.R
@@ -9,15 +9,15 @@ get_biogeoclimatic_zone_polygons <- function(study_fire_sampling_polygons) {
     # Dissolve into single MULTIPOLYGON for each BGZ
     dplyr::group_by(ZONE_NAME) %>% 
     dplyr::summarise(
-      bgz = unique(ZONE),
+      bgz_short = unique(ZONE),
       bgz_name = unique(ZONE_NAME),
       geometry = sf::st_union(geometry),
       .groups = "drop"
     ) %>%
     # Add a unique numerical code
-    dplyr::mutate(bgz_code = as.numeric(as.factor(bgz))) %>% 
+    dplyr::mutate(bgz = as.numeric(as.factor(bgz_short))) %>% 
     # Clean-up
-    dplyr::select(bgz, bgz_code, bgz_name)
+    dplyr::select(bgz, bgz_short, bgz_name)
   
   # Return polygons
   return(biogeoclimatic_zone_polygons)
